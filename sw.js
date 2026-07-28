22fbf899f08cd8da938c15f031305de3c5843f9f
Staś <83009119+neworldheh@users.noreply.github.com>
2026-07-29 00:15:20 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "loan-v1";
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache =>
+            cache.addAll([
+                "./",
+                "./index.html",
+                "./manifest.webmanifest"
+            ])
+        )
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
